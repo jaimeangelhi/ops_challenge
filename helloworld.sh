458cd6eba35e0016b0e3db6ddd46b9f8309f1506
jaimeangelhi <jai.me.angel.hi@gmail.com>
2023-10-24 10:50:44 -1000

    Added helloworld file and bash script

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  helloworld
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      10/24/2023
+# Purpose:                      to print "hello world" to the screen.
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+echo helloworld
+
+# End
