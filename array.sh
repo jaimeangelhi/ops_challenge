e8e3e59e692d51a2cc76791d46bc272fe309ecde
jaimeangelhi <jai.me.angel.hi@gmail.com>
2023-10-26 08:25:44 -1000

    added new concat.sh

---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script Name:                  array.sh
+# Author Name:                  jai.me.angel.hi
+# Date of latest revision:      10.26.2023
+# Purpose:                      Demo array
+# Execution:                    bash array.sh or ./array.sh chmod -x array.sh
+# Additional Sources:           X
