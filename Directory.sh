91df2e605b10513f5b6e47ee1d507376791d114a
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2023-10-26 18:26:18 -1000

    Create Directory.sh
    Initial commit

---
+++ Directory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script Name:                  Directory
+# Author:                       Jai.me.angel.hi
+# Date of latest revision:      10/26/2023
+# Purpose:                      Create an array directory
+
+# Declaration of variables
+dir1="dir1"
+dir2="dir2"
+dir3="dir3"
+dir4="dir4"
+
+touch dir1
+touch dir2
+touch dir3
+touch dir4
+
+
+# Declaration of functions
+
+directory=(dir1 dir2 dir3 dir4)
+
+# Main
+
+
+
+# End
