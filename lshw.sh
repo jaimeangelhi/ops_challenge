642f04da05cebb53ba4d64e97f4156fcfe5b92f9
jaimeangelhi <jai.me.angel.hi@gmail.com>
2023-11-01 13:57:30 -1000

    updated lshw.sh

---
+++ lshw.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  lshw
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      11/1/2023
+# Purpose:                      Displays sys info to screen about Computer Name, CPU, RAM, DISPLAY ADAPTER, and NETWORK ADAPTER
+# Additional Sources:           Class Repo GitHub
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
