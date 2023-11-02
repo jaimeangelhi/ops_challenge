304d77e89ff761f12d5f731d8811a89fe8d70186
jaimeangelhi <jai.me.angel.hi@gmail.com>
2023-11-01 14:23:01 -1000

    updated lshw.sh

--- lshw.sh
+++ lshw.sh
@@ -4,7 +4,7 @@
 # Author:                       jai.me.angel.hi
 # Date of latest revision:      11/1/2023
 # Purpose:                      Displays sys info to screen about Computer Name, CPU, RAM, DISPLAY ADAPTER, and NETWORK ADAPTER
-# Additional Sources:           Class Repo GitHub, Class review Demo - Roger Huba
+# Additional Sources:           Class Repo GitHub; Class review Demo - Roger Huba, Marcus Nogueira, Ian Bennett
 
 
 # Declaration of variables
@@ -14,13 +14,14 @@
 
 
 # Main
-
+figlet "jaha"
+echo ""
 echo "Evaluating this computer..."
-echo "Computer Name: "$(lshw | grep "" -m1)
-echo "CPU: "$(lshw | grep "*-cpu" -A 5)
-echo "RAM: "$(lshw | grep "*-memory" -A 3)
-echo "Display Adapter: "$(lshw | grep "*-display" -A 10)
-echo "Network Adapter: "$(lshw | grep "*-network" -A 15)
+echo -e "Computer Name:\n\n "$(lshw | grep "" -m1)
+echo -e "CPU:\n\n "$(lshw | grep "*-cpu" -A 5)
+echo -e "RAM:\n\n "$(lshw | grep "*-memory" -A 3)
+echo -e "Display Adapter:\n\n "$(lshw | grep "*-display" -A 10)
+echo -e "Network Adapter:\n\n "$(lshw | grep "*-network" -A 15)
 echo "Evaluation complete."
 
 
