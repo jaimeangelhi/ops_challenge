135d07d50e83acb2341bcea3ef4c6ac9323fcc95
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2023-11-01 14:08:02 -1000

    Update lshw.sh

--- lshw.sh
+++ lshw.sh
@@ -4,7 +4,7 @@
 # Author:                       jai.me.angel.hi
 # Date of latest revision:      11/1/2023
 # Purpose:                      Displays sys info to screen about Computer Name, CPU, RAM, DISPLAY ADAPTER, and NETWORK ADAPTER
-# Additional Sources:           Class Repo GitHub
+# Additional Sources:           Class Repo GitHub, Class review Demo - Roger Huba
 
 
 # Declaration of variables
@@ -15,6 +15,13 @@
 
 # Main
 
+echo "Evaluating this computer..."
+echo "Computer Name: "$(lshw | grep "" -m1)
+echo "CPU: "$(lshw | grep "*-cpu" -A 5)
+echo "RAM: "$(lshw | grep "*-memory" -A 3)
+echo "Display Adapter: "$(lshw | grep "*-display" -A 10)
+echo "Network Adapter: "$(lshw | grep "*-network" -A 15)
+echo "Evaluation complete."
 
 
 # End
