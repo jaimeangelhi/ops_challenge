132f9f603de155f2803060027c5e39371b724a90
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2023-11-12 14:32:05 -1000

    Update Directory.sh accomplished

--- Directory.sh
+++ Directory.sh
@@ -4,18 +4,17 @@
 # Author:                       Jai.me.angel.hi
 # Date of latest revision:      10/26/2023
 # Purpose:                      Create an array directory
+# Additional Source             ChatGPT (https://chat.openai.com/share/245ae3ec-6ba5-42fa-ae6b-15ec21d25730)
 
-# Declaration of variables
 
-dirs=( dir1 dir2 dir3 dir4 )
-# Declaration of functions
+# Create four directories
+mkdir dir1 dir2 dir3 dir4
 
-directory=() {
-    mkdir $dirs
-}
+# Put the names of the directories in an array
+directories=("dir1" "dir2" "dir3" "dir4")
 
-# Main
-directory
-touch "$dirs/textfile.txt"
-
-# End
+# Loop through the array and create a new .txt file in each directory
+for dir in "${directories[@]}"; do
+    touch "$dir/file.txt"
+    echo "File created in $dir"
+done
