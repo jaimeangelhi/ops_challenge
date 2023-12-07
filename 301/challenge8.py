6b922fbcb8f4c76330be8b95de80ccea183e8b6a
jaimeangelhi <jai.me.angel.hi@gmail.com>
2023-12-06 16:13:36 -1000

    add new challenge8

---
+++ 301/challenge8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Script Name:                  challenge8.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      12/6/2023
+# Purpose:                      Making, printing, and editing a list
+# Additional Source             ChatGPT
+
+# Assigning a list of ten string elements to a variable
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+print(my_list)
+
+# Print the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements:", my_list[5:])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+# Print the updated list
+print("Updated list:", my_list)
