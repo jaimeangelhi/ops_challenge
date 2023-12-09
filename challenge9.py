54ab2e9126dbcafa251cd1ecd5e0eb6275c4888e
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2023-12-08 15:17:52 -1000

    Update challenge9.py

--- challenge9.py
+++ challenge9.py
@@ -1,2 +1,35 @@
-!#
+#!/usr/bin/env python3
 
+# Script Name:                  challenge9.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      12/7/2023
+# Purpose:                      Create Python conditional statements
+# Additional Source             ChatGPT
+
+# Assign values to variables a and b
+a = 10
+b = 5
+
+# Example 1: Using if and elif
+if a == b:
+    print("a is equal to b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is less than b")
+
+# Example 2: Using different logical conditionals
+if a != b:
+    print("a is not equal to b")
+
+if a < b:
+    print("a is less than b")
+
+if a <= b:
+    print("a is less than or equal to b")
+
+if a > b:
+    print("a is greater than b")
+
+if a >= b:
+    print("a is greater than or equal to b")
