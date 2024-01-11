8b3aa6147aa903ab9aeda03eb02103d825d71c2e
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2024-01-10 21:34:50 -1000

    Create ops-401d10-challenge03.py

---
+++ 401/ops-401d10-challenge03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Script Name:                  ops-401d10-challenge03.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      1/10/24
+# Purpose:                      Uptime sensor in python emailed
+# Additional Source             ChatGPT (
+
+
