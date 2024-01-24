d9d640dfa6c0711f799c5aa73de516959e689d6c
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2024-01-23 21:53:53 -1000

    Create ops-401d10-challenge12.py

---
+++ 401/ops-401d10-challenge12.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Script Name:                  ops-401d10-challenge12.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      1/23/24
+# Purpose:                      Network Security Tool with Scapy Part 2 of 3
+# Additional Source             ChatGPT
