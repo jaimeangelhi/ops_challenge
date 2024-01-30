21d22006f507a56e0a6cee087ff1cec3d96bc332
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2024-01-29 21:50:38 -1000

    Create ops-401d10-challenge16.py

---
+++ 401/ops-401d10-challenge16.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Script Name:                  ops-401d10-challenge16.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      1/29/24
+# Purpose:                      Automated Brute Force Wordlist Attack Tool Part 1 of 3
+# Additional Source             ChatGPT, Class Demo
+
