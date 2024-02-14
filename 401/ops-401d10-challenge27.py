2dec91795b6ed7f026396621f53d0ecd3382aef1
jaimeangelhi <146782533+jaimeangelhi@users.noreply.github.com>
2024-02-13 20:24:26 -1000

    Create ops-401d10-challenge27.py

---
+++ 401/ops-401d10-challenge27.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Script Name:                  ops-401d10-challenge27.py
+# Author:                       jai.me.angel.hi
+# Date of latest revision:      2/13/24
+# Purpose:                      Event Logging Tool Part 2 of 3
+# Additional Source             ChatGPT (), Class Demo
+
+
